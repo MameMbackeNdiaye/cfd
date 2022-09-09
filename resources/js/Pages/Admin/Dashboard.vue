da15ecc0ea9c17f69d53e48e0a799bbc02266f1e
Mame Mbacke Ndiaye <mambacke0@gmail.com>
2022-09-09 23:40:53 +0000

    dashboard template

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,16 @@
+<script>
+import Argon from '@/Layouts/Argon.vue';
+export default {
+  components: {
+      Argon
+      
+  },
+ template: '#app',
+}
+</script>
+
+<template>
+<Argon></Argon>
+</template>
+
+
